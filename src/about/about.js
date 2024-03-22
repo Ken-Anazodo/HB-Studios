57e862530b297436751b578ce6368aab9149abfd
KENNETH ANAZODO <kenanazodo43@gmail.com>
2024-03-22 23:39:25 +0100

    nav animation rythm fixed

--- src/about/about.js
+++ src/about/about.js
@@ -21,17 +21,7 @@ const showAnim = gsap.from('.nav-sec-container', {
   // OPEN AND CLOSE NAV
 let menu = document.getElementById('menu-wrapper');
 let logoAndCloseMenu = document.getElementById('logoAndClose');
-// let menubtn = document.querySelector(".menubtn");
 
-// const closeMenu = () => {
-// 	menu.style.top = "-150vh";
-// 	logoAndCloseMenu.style.display = "none";
-// }
-
-// const openMenu = () => {
-// 	menu.style.top = "0";
-// 	logoAndCloseMenu.style.display = "flex";
-// }
 
 
 let tl = gsap.timeline()
@@ -49,8 +39,8 @@ const openMenu = () => {
 	tl.fromTo(menu, {top: '-150vh'}, {top: '0', duration: 0.5, ease: "power4.in"})
 	  .fromTo(".gNav", {y: '15vw'}, {y: 0, duration: 0.5, ease: "power4.in", transition: 1 }, "1")
       .fromTo(".navReveal", {height: "100%"}, {height: "0%", duration: 1, ease: 'power4.in'}, "1")
-      .fromTo(".navImgg", {y: '5vw', scale: 2}, {y: 0, scale: 1, transition: 1.5, duration: 1.5,}, "2") 
-	  .fromTo(".navImgg1", {y: '5vw', scale: 2}, {y: 0, scale: 1, transition: 1.5, duration: 1.5,}, "2.3") 
+	  .fromTo(".navImgg", {scale: 1.2}, {scale: 1, duration: 7,}, "2") 
+	  .fromTo(".navImgg1", {scale: 1.2}, {scale: 1, duration: 7,}, "2")  
       .fromTo(".hr", {width: "0"}, {width: '100%', duration: 2, ease: "power4.out"}, "2")  
     // menubtn.style.display = "none";
     logoAndCloseMenu.style.display = "flex";
