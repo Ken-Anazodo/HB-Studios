3806bd3bbb9d97124c198370d6818db634531ec2
KENNETH ANAZODO <kenanazodo43@gmail.com>
2024-04-01 13:25:10 +0100

    configuring webpack

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,18 @@ module.exports = {
       {
 		// Process CSS files
         test: /\.css$/, 
-        use: ['style-loader', 'css-loader'],
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+
+      {
+        test: /\.js$/, // apply this rule to .js files
+        exclude: /node_modules/, // don't apply to files in node_modules directory
+        use: {
+          loader: 'babel-loader', // use babel-loader for transpiling JavaScript files
+          options: {
+            presets: ['@babel/preset-env'], // use @babel/preset-env to transpile ES6+ syntax
+          },
+        },
       },
       // Add more rules for other asset types if needed
     ],
