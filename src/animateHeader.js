6608aa4cc260e19b79cbce90ea55c9f2a8a64f58
KENNETH ANAZODO <kenanazodo43@gmail.com>
2024-04-17 21:27:14 +0100

    configuring newsLetter section

--- src/animateHeader.js
+++ src/animateHeader.js
@@ -1,25 +1,25 @@
 // Animate Headers
-export const animateHeaders = (headerId, subHeader, start) => {
+// const animateHeaders = (headerId, subHeader, start) => {
       
-	document.addEventListener("DOMContentLoaded", (event) => {
-	  gsap.registerPlugin(CSSRulePlugin);
+// 	document.addEventListener("DOMContentLoaded", (event) => {
+// 	  gsap.registerPlugin(CSSRulePlugin);
 	
-	  const text = new SplitType(headerId, { types: 'lines' })
-	  var rule = CSSRulePlugin.getRule(".line:after");
+// 	  const text = new SplitType(headerId, { types: 'lines' })
+// 	  var rule = CSSRulePlugin.getRule(".line:after");
 
-	  gsap.fromTo(subHeader, {opacity: 0, y: "-5vw"}, {opacity: 1, duration: 1, y: 0,  
-		  scrollTrigger:{
-		  trigger: subHeader,
-		  start: start,
-		}})
+// 	  gsap.fromTo(subHeader, {opacity: 0, y: "-5vw"}, {opacity: 1, duration: 1, y: 0,  
+// 		  scrollTrigger:{
+// 		  trigger: subHeader,
+// 		  start: start,
+// 		}})
 		
-	  gsap.to(rule, {cssRule: {scaleY: 0}, duration: 1,  
-		scrollTrigger:{
-		trigger: subHeader,
-		start: start,
-	  }});
-	});
+// 	  gsap.to(rule, {cssRule: {scaleY: 0}, duration: 1,  
+// 		scrollTrigger:{
+// 		trigger: subHeader,
+// 		start: start,
+// 	  }});
+// 	});
 
-  }
+//   }
 
-  animateHeaders()
+//   animateHeaders()
